814930b108b6cc66020d82d0141a8a029970e03c
Marc Addeo <marcaddeo@gmail.com>
2022-10-27 09:17:25 -0400

    Use anyhow for error handling

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use confique::{Config, yaml::FormatOptions};
 use std::collections::HashMap;
 use clap::{Parser, Subcommand};
 use std::fs::File;
+use anyhow::{Result, Context};
 
 mod ymd_hm_format;
 
@@ -72,21 +73,21 @@ struct Record {
     work_description: String,
 }
 
-fn read_csv(file: String) -> Vec<Record> {
+fn read_csv(file: String) -> Result<Vec<Record>> {
     let mut records: Vec<Record> = vec![];
 
     let iordr: Box<dyn io::Read> = if file == "-" {
         Box::new(io::stdin())
     } else {
-        Box::new(File::create(file).unwrap())
+        Box::new(File::open(file)?)
     };
 
     let mut rdr = csv::Reader::from_reader(iordr);
     for result in rdr.deserialize() {
-        records.push(result.unwrap());
+        records.push(result?);
     }
 
-    records
+    Ok(records)
 }
 
 fn print_config_template() {
@@ -94,19 +95,25 @@ fn print_config_template() {
     println!("{}", yaml);
 }
 
-fn process_csv(cli: Cli) {
-    let config = Conf::from_file("config.yml").unwrap();
+fn process_csv(cli: Cli) -> Result<()> {
+    let config = Conf::from_file("config.yml")?;
     let client = reqwest::blocking::Client::new();
     let mut tw = TabWriter::new(io::stdout()).minwidth(2).padding(2);
 
-    let records = read_csv(cli.file);
+    let records = read_csv(cli.file.clone())
+        .with_context(|| {
+            if cli.file == "-" {
+                format!("Failed to read csv data from STDIN")
+            } else {
+                format!("Failed to read csv data from {}", cli.file)
+            }
+        })?;
     for record in records {
         write!(
             &mut tw,
             "{}\t // {}\t {}\t {}h\t ... ",
             record.issue_key, record.issue_summary, record.work_description, record.hours
-        )
-        .unwrap();
+        )?;
 
         let project_id = match config.project_map.get(&record.project_key) {
             Some(id) => id,
@@ -115,8 +122,7 @@ fn process_csv(cli: Cli) {
                     &mut tw,
                     "Could not map project: {};\t skipped.\n",
                     record.project_key
-                )
-                .unwrap();
+                )?;
 
                 continue;
             }
@@ -124,7 +130,7 @@ fn process_csv(cli: Cli) {
 
         let json = json!({
             "start": record.work_date,
-            "end": record.work_date + FloatDuration::hours(record.hours).to_chrono().unwrap(),
+            "end": record.work_date + FloatDuration::hours(record.hours).to_chrono()?,
             "projectId": project_id,
             "description": format!("{}: {}", record.issue_key, record.work_description),
         });
@@ -136,25 +142,25 @@ fn process_csv(cli: Cli) {
                 .post(api_url)
                 .header("X-Api-Key", config.api_key.clone())
                 .json(&json)
-                .send()
-                .unwrap();
+                .send()?;
 
             match response.error_for_status() {
-                Ok(_) => write!(&mut tw, "success.").unwrap(),
-                Err(_) => write!(&mut tw, "error.").unwrap(),
+                Ok(_) => write!(&mut tw, "success.")?,
+                Err(_) => write!(&mut tw, "error.")?,
             }
         } else {
-            write!(&mut tw, "dry run.").unwrap();
+            write!(&mut tw, "dry run.")?;
         }
 
-        write!(&mut tw, "\n").unwrap();
+        write!(&mut tw, "\n")?;
     }
 
-    tw.flush().unwrap();
+    tw.flush()?;
 
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
@@ -162,7 +168,9 @@ fn main() {
             print_config_template()
         },
         None => {
-            process_csv(cli)
+            process_csv(cli)?
         }
     }
+
+    Ok(())
 }
